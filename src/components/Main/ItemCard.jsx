4eb14a938f0230600455694ceb571af49a018626
federicoacker <acker.federico@gmail.com>
2026-05-18 19:28:00 +0200

    Aggiunto rotazione alle card per ora

--- src/components/Main/ItemCard.jsx
+++ src/components/Main/ItemCard.jsx
@@ -1,31 +1,43 @@
+import { useState } from "react"
 import { Card, Col } from "react-bootstrap"
 import { Tooltip } from "react-tooltip"
 
-function ItemCard({ title, name, original_language, original_title, vote_average, imageSrc }) {
-    console.log(name);
+function ItemCard({ title, original_language, original_title, vote_average, imageSrc }) {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <Col xs={12} sm={12} md={6} xl={4} data-bs-theme="dark">
-            <Card className="h-100">
-                <Card.Header className="d-flex flex-column row-gap-3">
-                    <Card.Img variant="top" src={imageSrc}/>
-                    <Card.Title>
-                        <h3 className="text-center">{title}</h3>
-                    </Card.Title>
-                </Card.Header>
-                <Card.Body className="d-flex flex-column">
-                    <Card.Text className="flex-grow-1">
-                        {original_title !== title && <p>Titolo Originale: {original_title}</p>}
-                        Lingua Originale: <span
-                            className={`lang-icon lang-icon-${original_language}`}
-                            data-tooltip-id="language-tooltip"
-                            data-tooltip-content={original_language}
-                        />
-                    </Card.Text>
-                    <Tooltip id="language-tooltip" />
-                    <Card.Text>
-                        Voto: {Array.from({length:vote_average}, (element ,index) => <i key={index} className="bi bi-star-fill"></i>)}
-                    </Card.Text>
-                </Card.Body>
+        <Col xs={12} sm={12} md={6} xl={4} data-bs-theme="dark" className="card-container">
+            <Card className={`h-100 result-card front ${isHovered && "flipped"}`} onMouseEnter={() => { setIsHovered(true) }} onMouseLeave={() => { setIsHovered(false) }}>
+                {isHovered &&
+                    <div className="back">
+                        <Card.Header className="d-flex flex-column row-gap-3">
+                            <Card.Title>
+                                <h3 className="text-center">{title}</h3>
+                            </Card.Title>
+                        </Card.Header>
+                        <Card.Body className="d-flex flex-column">
+                            <Card.Text className="flex-grow-1">
+                                {original_title !== title && 
+                                <span>
+                                    Titolo Originale: {original_title}
+                                    <br/>
+                                </span>
+                                }
+                                
+                                Lingua Originale: <span
+                                    className={`lang-icon lang-icon-${original_language}`}
+                                    data-tooltip-id="language-tooltip"
+                                    data-tooltip-content={original_language}
+                                />
+                            </Card.Text>
+                            <Tooltip id="language-tooltip" />
+                            <Card.Text>
+                                Voto: {Array.from({ length: vote_average }, (element, index) => <i key={index} className="bi bi-star-fill"></i>)}
+                            </Card.Text>
+                        </Card.Body>
+                    </div>
+                }
+                {!isHovered ? <img src={imageSrc} alt={title} className="img-fluid posterImage front"/> : null}
             </Card>
         </Col>
     )
--- src/components/Main/ItemCardList.jsx
+++ src/components/Main/ItemCardList.jsx
@@ -5,11 +5,10 @@ function ItemCardList({data, isLoaded}) {
 
     return (
         <Row className="row-gap-2 py-4">
-            {(isLoaded) && data?.map(({ id, title, name, original_language, original_title, vote_average, imageSrc }) => {
+            {(isLoaded) && data?.map(({ id, title, original_language, original_title, vote_average, imageSrc }) => {
                 return <ItemCard
                     key={id}
                     title={title}
-                    name={name}
                     original_language={original_language}
                     original_title={original_title}
                     vote_average={vote_average}
