3ed75ddf85380aa34f5583da8680011d4a3d7d95
federicoacker <acker.federico@gmail.com>
2026-05-18 12:07:10 +0200

    Creato un custom hook useSearch per fare la search dei movies

---
+++ src/hooks/useSearch.js
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const API_URL = "https://api.themoviedb.org/3/search/movie";
+
+function useSearch(query){
+    const [data, setData] = useState({});
+    const [loadingError, setLoadingError] = useState("");
+    const [isLoaded, setIsLoaded] = useState(false);
+    
+    const options = {
+        method:'GET',
+        headers:{
+            accept: 'application/json',
+            Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}.${import.meta.env.VITE_TMDP_READ_ACCESS_TOKEN}`
+        }
+    };    
+    
+    useEffect(() => {
+        
+        fetch(`${API_URL}?query=${query}&include_adult=false&language=it-IT`, options)
+        .then(response => response.json())
+        .then(json => {
+            setIsLoaded(true);
+            setData(json);
+        })
+        .catch(error => {
+            setIsLoaded(false);
+            setLoadingError(error);
+        })
+    }, [query]
+    );
+
+    return {
+        data,
+        loadingError,
+        isLoaded
+    }
+}
+
+export default useSearch
