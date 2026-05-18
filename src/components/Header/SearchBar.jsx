7a47210da8867e64916329396389e85a8f9007b2
federicoacker <acker.federico@gmail.com>
2026-05-18 12:21:20 +0200

    Verificato il funzionamento del custom hook per il debounce, adesso passo a centralizzare i dati in un context

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,25 +1,20 @@
 import { useState } from "react";
 import useSearch from "../../hooks/useSearch.js";
+import useDebounce from "../../hooks/useDebounce.js";
 
 function SearchBar() {
     const [queryValue, setQueryValue] = useState("");
-    const [submittedQueryValue, setSubmittedQueryValue] = useState("");
-    const {data, loadingError, isLoaded} = useSearch(submittedQueryValue);
+    const debouncedValue = useDebounce(queryValue, 500);
+    const {data, loadingError, isLoaded} = useSearch(debouncedValue);
 
     const changeHandler = (event) => {
         setQueryValue(event.target.value);
     }
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        setSubmittedQueryValue(queryValue);
-        setQueryValue("");
-    }
+
     return (
-        <form className="d-flex gap-2" onSubmit={submitHandler}>
+        <form className="d-flex gap-2">
             <input className="form-control" type="text" name="query" value={queryValue} onChange={changeHandler} placeholder="Search movies.." />
-            <button className="btn btn-danger">Search</button>
-            {JSON.stringify(data)}
         </form>
     )
 }
